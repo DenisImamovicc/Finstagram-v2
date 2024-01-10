0d9755291cac02095b1c5878ee77d8b1db2b4fd6
Denis Imamovic <Danielarbete@protonmail.com>
2024-01-10 23:27:25 +0100

    final responsive design,still not happy w footer when no cards :(

--- src/main.ts
+++ src/main.ts
@@ -15,13 +15,15 @@ searchImgForm.addEventListener("submit",(e) => {
 
 const renderImages = (images) => {
   images.map((image) => {
-    imageList.innerHTML += `          
-    <div class="card m-2" style="width: 18rem;">
-      <img src="https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg" class="card-img-top" alt="${image.title}">
-      <div class="card-body">
-        <h5 class="card-title">${image.title}</h5>
+    imageList.innerHTML += `
+    <div class="col">       
+      <div class="card m-2 h-100">
+        <img src="https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg" class="card-img w-100 h-100" alt="${image.title}">
+        <div class="card-img-overlay ">
+          <h5 class="card-title bg-dark text-white p-1 rounded d-inline" text-break>${image.title}</h5>
+        </div>
       </div>
-    </div>
+    </div>   
   `
   })
 }
