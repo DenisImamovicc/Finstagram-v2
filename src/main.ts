aacd973738d4023f7ae6fc64690c68906565ae0e
Denis Imamovic <Danielarbete@protonmail.com>
2024-01-10 16:01:29 +0100

    Add dynaic rendering of cards amount and content depending on req data,currently from a file to simulate req to flickr api for photos with user search input

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,27 @@
 import './style.css'
+import data from "./data.json"
 
+
+const imageList = document.querySelector("#Imagelist") as HTMLDivElement
+const searchImgForm = document.querySelector("#Searchimgform") as HTMLFormElement
+
+searchImgForm.addEventListener("submit",(e) => {
+  e.preventDefault()
+  // const currUserSearch = e.target[0].value
+  console.log(data);
+  renderImages(data.photos.photo)
+})
+
+
+const renderImages = (images) => {
+  images.map((image) => {
+    imageList.innerHTML += `          
+    <div class="card m-2" style="width: 18rem;">
+      <img src="https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg" class="card-img-top" alt="${image.title}">
+      <div class="card-body">
+        <h5 class="card-title">${image.title}</h5>
+      </div>
+    </div>
+  `
+  })
+}
