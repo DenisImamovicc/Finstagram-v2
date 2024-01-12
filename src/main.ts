cc9641789dcc0b0ea64b9f71c910e26be68bb218
Denis Imamovic <Danielarbete@protonmail.com>
2024-01-12 14:42:07 +0100

    Add load feature to show user when new or additional content is getting to be rendered

--- src/main.ts
+++ src/main.ts
@@ -6,31 +6,32 @@ const imageList = document.querySelector("#Imagelist") as HTMLDivElement
 const searchImgForm = document.querySelector("#Searchimgform") as HTMLFormElement
 const submitBttn = document.querySelector("#submitBttn") as HTMLButtonElement
 const Searchinput = document.querySelector("#Searchinput") as HTMLInputElement
+const Listloader = document.querySelector("#Listloader") as HTMLDivElement
 
 const API_KEY = import.meta.env.VITE_APP_FLICKR_API_KEY
 const FLICKR_URL = import.meta.env.VITE_APP_FLICKR_URL
 let prevUserSearch : null | string = null
 let FlickrData:FlickrResponse
 
-window.addEventListener("scrollend", async () =>{
+window.addEventListener("scrollend", async () => {
  if (window.scrollY + window.innerHeight > document.documentElement.scrollHeight - 200) {
+  Listloader.hidden = false
    let data = await getPhotosBySearch(`${FLICKR_URL}&api_key=${API_KEY}&text=${Searchinput.value}&per_page=10&page=${++FlickrData.photos.page}&format=json&nojsoncallback=1`)
    renderImages(data.photos)
+   Listloader.hidden = true
  }
-//Display loading el when user triggers next side req and disspar when req is done.Also show user total pages of result and curr page it loads
 })
 
 searchImgForm.addEventListener("submit", async (e) => {
   e.preventDefault()
-
-
   if (Searchinput.value.length === 0 || Searchinput.value === prevUserSearch) {
     return renderTempAlert("Cannot send empty or previous search word!","alert-warning")
   }else{
     prevUserSearch = Searchinput.value
   }
   submitBttn.disabled = true
-
+  imageList.innerHTML = ""
+  Listloader.hidden = false
   try {
     FlickrData = await getPhotosBySearch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&text=${Searchinput.value}&per_page=10&page=1&format=json&nojsoncallback=1`)
   } catch (error) {
@@ -38,12 +39,10 @@ searchImgForm.addEventListener("submit", async (e) => {
     submitBttn.disabled = false
   }
   console.log(FlickrData);
-  imageList.innerHTML = ""
   renderImages(FlickrData.photos)
   submitBttn.disabled = false
-
+  Listloader.hidden = true
 })
-
 
 const renderImages = (images:PhotosData) => {
   images.photo.map((image:Photo) => {
